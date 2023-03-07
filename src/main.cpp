6f56e2fc663082f94cf65f047a08745b6b58913e
joetorelli <josephtorelli@gmail.com>
2023-03-07 11:21:42 -0600

    TemplateV3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 /******************************************************
  * Project:
- * Author:
  * Description:
 
  *******************************************************/
@@ -15,46 +14,98 @@
 *******************************************************/
 
 /******************************************************
- * I/O Ports used
-
+ **** I/O Ports used
+ 
 *******************************************************/
+
+/************************  end of notes ****************/
 
 /******************************************************
- * Includes                                           
-*******************************************************/
+ **** Includes
+ *******************************************************/
 #include <Arduino.h>
+// #include <Preferences.h> //NVM
 
 /******************************************************
- * Definitions
-*******************************************************/
+ **** Pin Definitions
+ *******************************************************/
+// assign i2c pin numbers
+// #define I2c_SDA 23
+// #define I2c_SCL 22
 
 /******************************************************
- * Global Vars
-*******************************************************/
+ **** Global Vars
+ *******************************************************/
+// const byte ON = 1;
+// const byte OFF = 0;
 
 /******************************************************
- * Timers
-******************************************************/
+ **** Timers
+ ******************************************************/
+
+/******************************************************
+ **** Init hardware
+ ******************************************************/
+/******************* eeprom ******************/
+// Preferences Settings; // NVM
 
 /*****************************************************
- * Sub/Function Declarations
-******************************************************/
+ **** Sub/Function Declarations
+ ******************************************************/
 
 /*****************************************************
- * SetUP
-******************************************************/
+ **** SetUP
+ ******************************************************/
 void setup()
 {
+
+	// serial ports
+	//   Serial1.begin(9600); // tx/rx pins
+	//   Serial.begin(57600); // usb
+	//   Serial.println("Serial 0 Start");
+	//   Serial1.println("Serial 1 Start");
+
+	/***************************** pin properties  ******************/
+
+	/********************   init i2c  *****************/
+	// Wire.begin(I2c_SDA, I2c_SCL);
+	//  bool status; // connect status
+	// Serial.println("I2C INIT OK");
+
+	/*****************************   NVM   ***************************/
+	// test for first run time
+/* 	Settings.begin("storage", RO_MODE); // nvm storage space, set to read
+	bool doesExist = Settings.isKey("NVSInit");
+	if (doesExist == false)
+	{
+
+		Serial1.println("-----------------NVM first time----------------");
+		// first time run code to create keys & assign their values
+		Settings.end();						// close the namespace in RO mode.
+		Settings.begin("storage", RW_MODE); //  create and open it in RW mode.
+
+		// load NVM with default values
+		Settings.putInt("Value Name 1", 2000);
+		Settings.putInt("Value Name 2", 1500);
+
+		Settings.putInt("NVSInit", true);
+		Settings.end();						// close the namespace
+		Settings.begin("storage", RO_MODE); // nvm storage space, set to read
+	}
+
+	// load NVM into vars
+	Value Name 1 = Settings.getInt("Value Name 1");
+	Value Name 2 = Settings.getInt("Value Name 2");
+	Settings.end(); // close the namespace */
 }
 
 /*****************************************************
- * Loop
-******************************************************/
+ **** Run Loop
+ ******************************************************/
 void loop()
 {
 }
 
-
-/*****************************************************
- * Subs/Function
-******************************************************/
+/******************************************************
+ **** Subs/Function
+ ******************************************************/
